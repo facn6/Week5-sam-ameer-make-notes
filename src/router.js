31f73e6374374d6b6c8c2bf537d27fc4f94483d1
Sam Harris <sam@jam-bc.com>
2019-07-31 16:51:59 +0300

    fixed api calls to use dynamic url and to provide query details relates #3

--- src/router.js
+++ src/router.js
@@ -1,15 +1,17 @@
 const url = require('url');
+const querystring = require('querystring');
 const handlers = require('./handlers.js');
 
 const router = (req, res) => {
-  const { pathname } = url.parse(req.url);
+  const { pathname, query } = url.parse(req.url);
 
-  if (pathname === '/' || pathname === '/style.css' || pathname === '/app.js' || pathname === '/assets/search-icon.png') {
+  if (pathname === '/' || pathname === '/style.css' || pathname === '/app.js') {
     handlers.homeHandler(req, res);
   } else if (pathname === '/api/assets') {
     handlers.returnAssets(req, res);
-  } else if (pathname === '/api/transcribe') {
-    handlers.returnTranscription('ameer-test.wav', res);
+  } else if (pathname.includes('/api/transcribe')) {
+    const { file } = querystring.parse(query);
+    handlers.returnTranscription(file, res);
   } else {
     handlers.errorHandler(req, res);
   }
